54a82d5acac2fea4f616267cbb65121c1be278bf
aymanamm <ayo12ammar24@gmail.com>
2022-03-11 18:51:17 +0100

    remove title

--- app/models.py
+++ app/models.py
@@ -1,4 +1,3 @@
-from turtle import title
 from xmlrpc.client import Boolean
 from sqlalchemy import TIMESTAMP, Column, ForeignKey, Integer, String, Boolean
 from sqlalchemy.sql.expression import text
